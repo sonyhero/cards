2a0efa8bd74aa6f9925e68eded279bfd22082d4a
sonyhero <27anton06@gmail.com>
2023-07-19 11:53:53 +0300

    add-checkbox-component-and-stories-add-dependencies

---
+++ src/components/ui/checkbox/checkbox.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { CheckboxDemo } from './'
+
+const meta = {
+  title: 'Components/CheckboxDemo',
+  component: CheckboxDemo,
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      options: ['default', 'withText'],
+      control: { type: 'radio' },
+    },
+  },
+} satisfies Meta<typeof CheckboxDemo>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const ShowCheckbox: Story = {
+  args: {
+    variant: 'default',
+  },
+}
+
+export const DisabledCheckbox: Story = {
+  args: {
+    isChecked: true,
+    isDisabled: true,
+    variant: 'default',
+  },
+}
+
+export const CheckboxWithText: Story = {
+  args: {
+    isChecked: false,
+    variant: 'withText',
+    checkBoxText: 'Test',
+  },
+}
+
+export const DisabledCheckboxWithText: Story = {
+  args: {
+    isChecked: false,
+    variant: 'withText',
+    isDisabled: true,
+    checkBoxText: 'Test',
+  },
+}
