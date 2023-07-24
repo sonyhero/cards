b4f41b21464d6867bb8edf5a3f7ce6e03ca43b3e
sonyhero <27anton06@gmail.com>
2023-07-24 16:08:58 +0300

    fix: textField

--- src/components/ui/textfield/textfield.tsx
+++ src/components/ui/textfield/textfield.tsx
@@ -1,26 +1,33 @@
-import { ComponentPropsWithoutRef, forwardRef, useState } from 'react'
+import { ChangeEvent, ComponentPropsWithoutRef, forwardRef, useState } from 'react'
 
-import { Eye, NotEye, Search } from '../../../common/assets/img'
+import { DeleteIcon, Eye, NotEye, Search } from '../../../common/assets/img'
 import { Typography } from '../typography'
 
 import s from './textfield.module.scss'
 
 export type TextFieldProps = {
-  type: 'default' | 'password' | 'search'
+  type: 'default' | 'password' | 'searchType'
   errorMessage?: string
   placeholder?: string
   disableValue?: boolean
 } & ComponentPropsWithoutRef<'input'>
 
 export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
-  ({ errorMessage, placeholder = 'TextField', type, disableValue = false, ...restProps }) => {
+  ({
+    errorMessage,
+    placeholder = 'TextField',
+    type = 'default',
+    disableValue = false,
+    ...restProps
+  }) => {
     const [showPassword, setShowPassword] = useState(true)
+    const [value, setValue] = useState('')
 
     const finalType = getType(type, showPassword)
 
-    const inputStyle = (type: 'default' | 'password' | 'search') => {
-      if (type === 'search') {
-        return { paddingLeft: '2.56rem' }
+    const inputStyle = (type: 'default' | 'password' | 'searchType') => {
+      if (type === 'searchType') {
+        return { paddingLeft: '2.56rem', paddingRight: '35px' }
       } else if (type === 'password') {
         return { paddingRight: '35px' }
       } else {
@@ -28,10 +35,14 @@ export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
       }
     }
 
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+      setValue(e.currentTarget.value)
+    }
+
     return (
       <>
         <div className={s.fieldContainer}>
-          {type === 'search' && (
+          {type === 'searchType' && (
             <span className={s.search}>
               <Search fill={disableValue ? '#4c4c4c' : '#808080'} />
             </span>
@@ -41,12 +52,14 @@ export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
             placeholder={placeholder}
             type={finalType}
             disabled={disableValue}
+            onChange={onChangeHandler}
             style={inputStyle(type)}
+            value={value}
             {...restProps}
           />
           {type === 'password' && (
             <button
-              className={s.showPassword}
+              className={s.buttonAction}
               type={'button'}
               disabled={disableValue}
               onClick={() => setShowPassword(prev => !prev)}
@@ -56,6 +69,16 @@ export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
               ) : (
                 <NotEye fill={disableValue ? '#4c4c4c' : '#fff'} />
               )}
+            </button>
+          )}
+          {type === 'searchType' && (
+            <button
+              className={s.buttonAction}
+              type={'button'}
+              disabled={disableValue}
+              onClick={() => setValue('')}
+            >
+              <DeleteIcon fill={disableValue ? '#4c4c4c' : '#808080'} />
             </button>
           )}
         </div>
