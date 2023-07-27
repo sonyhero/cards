e1ca3fe3e781542c79d4e2035436b1258bd39ba7
sonyhero <27anton06@gmail.com>
2023-07-27 14:46:45 +0300

    fix: select and slider

--- src/components/ui/select/select.stories.tsx
+++ src/components/ui/select/select.stories.tsx
@@ -6,14 +6,25 @@ const meta = {
   title: 'Components/Select',
   component: SelectDemo,
   tags: ['autodocs'],
+  argTypes: { onChangeOption: { action: 'select changes' } },
 } satisfies Meta<typeof SelectDemo>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
+const people = [
+  { id: 1, value: 'Durward Reynolds' },
+  { id: 2, value: 'Kenton Towne' },
+  { id: 3, value: 'Therese Wunsch' },
+  { id: 4, value: 'Benedict Kessler' },
+  { id: 5, value: 'Katelyn Rohan' },
+]
+
 export const SelectStory: Story = {
   args: {
     isDisabled: false,
+    options: people,
+    value: people[0],
   },
 }
 
