57680ad9f90a3fe54e959a4bdf5cb9404dc74fe7
sonyhero <27anton06@gmail.com>
2023-07-28 23:36:09 +0300

    fix: label stories

--- src/components/ui/label/label.stories.tsx
+++ src/components/ui/label/label.stories.tsx
@@ -14,25 +14,22 @@ export default meta
 
 export const LabelStoryPrimary = () => {
   return (
-    <>
-      <LabelDemo label={'Input'} disabled={false} variant={'primary'} />
+    <LabelDemo label={'Input'} disabled={false} variant={'primary'}>
       <TextField type={'default'} value={'Input'} disabled={false} />
-    </>
+    </LabelDemo>
   )
 }
 export const LabelStorySecondary = () => {
   return (
-    <>
-      <LabelDemo label={'Input'} disabled={false} variant={'secondary'} />
+    <LabelDemo label={'Input'} disabled={false} variant={'secondary'}>
       <TextField type={'default'} value={'Input'} disabled={false} />
-    </>
+    </LabelDemo>
   )
 }
 export const LabelStoryDisabled = () => {
   return (
-    <>
-      <LabelDemo label={'Input'} disabled={true} />
+    <LabelDemo label={'Input'} disabled={true}>
       <TextField type={'default'} value={'Input'} disabled={true} />
-    </>
+    </LabelDemo>
   )
 }
