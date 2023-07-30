5fff73e403bdb8b0bb60eefcb6cace973ed3e384
sonyhero <27anton06@gmail.com>
2023-07-30 16:34:38 +0300

    fix: card

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -2,9 +2,10 @@ import { FC, ReactNode } from 'react'
 
 import s from './card.module.scss'
 type CardProps = {
+  className?: string
   children: ReactNode
 }
 
-export const Card: FC<CardProps> = ({ children }) => {
-  return <div className={s.cardBlock}>{children}</div>
+export const Card: FC<CardProps> = ({ children, className }) => {
+  return <div className={`${s.cardBlock} ${className}`}>{children}</div>
 }
