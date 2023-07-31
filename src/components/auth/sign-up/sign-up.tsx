7eec366a0e93b87d67f35cb3e75b4e654f8fcd1d
sonyhero <27anton06@gmail.com>
2023-07-31 15:10:17 +0300

    feat: sign up

---
+++ src/components/auth/sign-up/sign-up.tsx
@@ -0,0 +1,74 @@
+import { DevTool } from '@hookform/devtools'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button, Card, Typography, ControlledTextField } from '../../ui'
+
+import s from './sign-up.module.scss'
+
+const sigInSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(3),
+    confirmPassword: z.string().min(3),
+  })
+  .refine(data => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: "Password don't match",
+  })
+
+type SignInFormShem = z.infer<typeof sigInSchema>
+export const SignUp = () => {
+  const { control, handleSubmit } = useForm<SignInFormShem>({
+    resolver: zodResolver(sigInSchema),
+  })
+
+  const onSubmit = (data: SignInFormShem) => {
+    console.log(data)
+  }
+
+  return (
+    <Card className={s.signBlock}>
+      <Typography className={s.title} variant={'large'}>
+        Sign Up
+      </Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <DevTool control={control} />
+        <ControlledTextField
+          name={'email'}
+          label={'Email'}
+          type={'default'}
+          placeholder={'enter your email'}
+          control={control}
+          className={s.email}
+        />
+        <ControlledTextField
+          name={'password'}
+          label={'Password'}
+          type={'password'}
+          placeholder={'enter your password'}
+          control={control}
+          className={s.password}
+        />
+        <ControlledTextField
+          name={'confirmPassword'}
+          label={'Confirm password'}
+          type={'password'}
+          placeholder={'enter your password'}
+          control={control}
+          className={s.confirmPassword}
+        />
+        <Button fullWidth={true} className={s.submit} type="submit">
+          Sign Up
+        </Button>
+      </form>
+      <Typography variant={'body2'} className={s.question}>
+        Already have an account?
+      </Typography>
+      <Button as={'a'} variant={'link'} className={s.signIn}>
+        Sign In
+      </Button>
+    </Card>
+  )
+}
