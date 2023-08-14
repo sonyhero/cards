e866a4c96909158ae188b194ab95dd7f61ed1b53
sonyhero <27anton06@gmail.com>
2023-08-14 16:19:10 +0300

    fix: global

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,7 @@
+import type { Preview } from '@storybook/react'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import '../src/styles/index.scss'
-
-import type { Preview } from '@storybook/react'
 
 const preview: Preview = {
   parameters: {
