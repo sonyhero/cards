710289cf89205da3ef35ea66766670217f1a2fb4
sonyhero <27anton06@gmail.com>
2023-08-15 20:24:29 +0300

    feat: auth flow, loader, toast

--- src/components/auth/personal-information/personal-information.tsx
+++ src/components/auth/personal-information/personal-information.tsx
@@ -1,8 +1,9 @@
-import { FC, useState } from 'react'
+import { ChangeEvent, FC, useState } from 'react'
 
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import { z } from 'zod'
 
 import s from './personalInformation.module.scss'
@@ -10,6 +11,11 @@ import s from './personalInformation.module.scss'
 import { Edit, Logout } from '@/assets'
 import { Button, Card, ControlledTextField, Typography } from '@/components/ui'
 import { AvatarDemo } from '@/components/ui/avatar'
+import {
+  useLogoutMutation,
+  useResendVerificationEmailMutation,
+  useUpdateProfileMutation,
+} from '@/services/auth'
 
 const sigInSchema = z.object({
   name: z.string().trim().min(1),
@@ -21,17 +27,45 @@ type PropsType = {
   name?: string
   email?: string
   avatar?: string
-  logout: () => void
   update: (value: string) => void
+  isEmailVer?: boolean
+  userId: string
 }
 
-export const PersonalInformation: FC<PropsType> = ({ name, email, avatar, logout, update }) => {
+export const PersonalInformation: FC<PropsType> = ({
+  name,
+  email,
+  avatar,
+  update,
+  userId,
+  isEmailVer,
+}) => {
   const [editMode, setEditMode] = useState<boolean>(false)
+  const [updatePhoto] = useUpdateProfileMutation()
+  const [logout] = useLogoutMutation()
+  const [resendVerEmail] = useResendVerificationEmailMutation()
+
   const { control, handleSubmit } = useForm<SignInFormShem>({
     resolver: zodResolver(sigInSchema),
   })
+
+  const mainPhotoSelected = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length) {
+      const formData = new FormData()
+
+      formData.append('avatar', event.target.files[0])
+      updatePhoto(formData)
+    }
+  }
   const onSubmit = (data: SignInFormShem) => {
     update(data.name)
+  }
+
+  const logoutHandler = () => {
+    logout()
+      .unwrap()
+      .then(() => toast.success('Всего хорошего'))
+      .catch(() => toast.error('Что-то пошло не так'))
   }
 
   return (
@@ -43,9 +77,19 @@ export const PersonalInformation: FC<PropsType> = ({ name, email, avatar, logout
         <div className={s.avatar}>
           <AvatarDemo src={avatar} name={name} className={s.avatar} />
           {!editMode && (
-            <div className={s.avatarEdit}>
-              <Edit />
-            </div>
+            <label htmlFor="mainPhotoInput">
+              <div className={s.avatarEdit}>
+                <Edit />
+              </div>
+              <div className={s.inputContainer}>
+                <input
+                  type={'file'}
+                  id="mainPhotoInput"
+                  onChange={mainPhotoSelected}
+                  className={s.mainPhotoInput}
+                />
+              </div>
+            </label>
           )}
         </div>
       </div>
@@ -58,7 +102,7 @@ export const PersonalInformation: FC<PropsType> = ({ name, email, avatar, logout
             type={'default'}
             control={control}
             className={s.editNickName}
-            placeholder={'Nickname'}
+            placeholder={'Name'}
           />
           <Button
             fullWidth={true}
@@ -89,7 +133,27 @@ export const PersonalInformation: FC<PropsType> = ({ name, email, avatar, logout
           <Typography variant={'body2'} as={'span'} className={s.email}>
             {email}
           </Typography>
-          <Button as={Link} to="/login" variant={'secondary'} className={s.logout} onClick={logout}>
+          {!isEmailVer && (
+            <Button
+              variant={'primary'}
+              onClick={() =>
+                resendVerEmail({
+                  userId,
+                  html: `<h1>Hi, ##name##</h1><p>Click <a href="http://localhost:5173/confirm-email/##token##">here</a> to recover your password</p>`,
+                })
+              }
+            >
+              ver
+            </Button>
+          )}
+
+          <Button
+            as={Link}
+            to="/login"
+            variant={'secondary'}
+            className={s.logout}
+            onClick={logoutHandler}
+          >
             <Logout />
             <Typography variant={'subtitle2'}>Logout</Typography>
           </Button>
